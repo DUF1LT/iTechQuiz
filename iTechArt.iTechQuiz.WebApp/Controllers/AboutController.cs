4317828807bd7043fe8ca1462e96cf71626d3e37
Vadim <duf1lt@yandex.ru>
2021-12-16 14:30:09 +0300

    configure startup, add directories for controllers and view, add wwwroot

---
+++ iTechArt.iTechQuiz.WebApp/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iTechArt.iTechQuiz.WebApp.Controllers
+{
+    public class AboutController : Controller
+    {
+
+    }
+}
---
+++ iTechArt.iTechQuiz.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace iTechArt.iTechQuiz.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //GET: Index
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Get Home/Index");
+            return View();
+        }
+    }
+}
