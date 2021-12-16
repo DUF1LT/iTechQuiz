38617af055d60a00c179a5a1ec5600ca3dffb9ca
Vadim <duf1lt@yandex.ru>
2021-12-16 14:09:50 +0300

    init projects

---
+++ iTechArt.Repositories/IRepositoryAsync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iTechArt.Repositories
+{
+    interface IRepositoryAsync<T> : IDisposable where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(int id);
+        void CreateAsync(T item);
+        void UpdateAsync(T item);
+        void DeleteAsync(int id);
+        void SaveAsync();
+    }
+}
