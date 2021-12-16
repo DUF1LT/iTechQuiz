22724139efcf7638e24ebd62a42072c86ae75041
Vadim <duf1lt@yandex.ru>
2021-12-16 14:52:46 +0300

    create layout page, add viewimports, add user model, configure logging, add repository interfaces

--- iTechArt.iTechQuiz.WebApp/Controllers/AboutController.cs
+++ iTechArt.iTechQuiz.WebApp/Controllers/AboutController.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace iTechArt.iTechQuiz.WebApp.Controllers
 {
     public class AboutController : Controller
     {
+        private readonly ILogger<AboutController> _logger;
+        public AboutController(ILogger<AboutController> logger)
+        {
+            _logger = logger;
+        }
 
+        //GET: Index
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Get About/Index");
+            return View();
+        }
     }
 }
--- iTechArt.iTechQuiz.WebApp/Program.cs
+++ iTechArt.iTechQuiz.WebApp/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
 
 namespace iTechArt.iTechQuiz.WebApp
 {
@@ -13,6 +14,10 @@ namespace iTechArt.iTechQuiz.WebApp
     {
         public static void Main(string[] args)
         {
+            string outputTemplate = "{Timestamp:yyyy-MM-ddHH: mm: ss.fff}[{Level}] {Message}{NewLine}{Exception}";
+            Log.Logger = new LoggerConfiguration().WriteTo.File("log.txt",
+            rollingInterval: RollingInterval.Day, 
+            outputTemplate: outputTemplate).CreateLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,6 +26,6 @@ namespace iTechArt.iTechQuiz.WebApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).UseSerilog();
     }
 }
