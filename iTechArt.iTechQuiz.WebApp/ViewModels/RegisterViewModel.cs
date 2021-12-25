2771161c06655c0a9e3a4ed169f6bf35d6f3b887
Vadim <duf1lt@yandex.ru>
2021-12-25 18:22:26 +0300

    create registration view, add possibility to register

---
+++ iTechArt.iTechQuiz.WebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace iTechArt.iTechQuiz.WebApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Login (email)")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password mismatch")]
+        [Display(Name = "Repeat Password")]
+        public string RepeatPassword { get; set; }
+    }
+}
