8f5f67c293feb1fba95841c01fe4eed30aad582a
Vadim <duf1lt@yandex.ru>
2021-12-28 22:09:54 +0300

    create sidebar partial view, style views,

---
+++ iTechArt.iTechQuiz.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iTechArt.iTechQuiz.WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        //[HttpGet]
+        //public Task<IActionResult> Users()
+        //{
+        //    return 
+        //}
+    }
+}
+    
