2b8b67b5a5e6a5b7be2f59941b86440c36d0ba0d
Vadim <duf1lt@yandex.ru>
2022-01-04 20:18:26 +0300

    add possibility to edit user information (name, email, role) and user password

--- iTechArt.iTechQuiz.WebApp/ViewModels/UserViewModel.cs
+++ iTechArt.iTechQuiz.WebApp/ViewModels/UserViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace iTechArt.iTechQuiz.WebApp.ViewModels
 {
     public class UserViewModel
     {
         public Guid Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
         public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
         public string Email { get; set; }
+
+        public bool IsAdmin { get; set; }
+
         public string Role { get; set; }
+
     }
 }
