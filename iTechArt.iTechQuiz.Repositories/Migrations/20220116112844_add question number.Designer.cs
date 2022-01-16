19b8c17b812f892b8cddb22ef8ea26acfbc32e8d
Vadim <duf1lt@yandex.ru>
2022-01-16 14:29:10 +0300

    add question number to Question

--- iTechArt.iTechQuiz.Repositories/Migrations/20220116112844_add question number.Designer.cs
+++ iTechArt.iTechQuiz.Repositories/Migrations/20220116112844_add question number.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using iTechArt.iTechQuiz.Repositories.Context;
 
 namespace iTechArt.iTechQuiz.Repositories.Migrations
 {
     [DbContext(typeof(iTechQuizContext))]
-    partial class iTechQuizContextModelSnapshot : ModelSnapshot
+    [Migration("20220116112844_add question number")]
+    partial class addquestionnumber
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
