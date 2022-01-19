95f696071e390ecdec55e0eceb15b330e1e24b66
Vadim <duf1lt@yandex.ru>
2022-01-19 15:27:38 +0300

    create basic view of new survey and style it

---
+++ iTechArt.iTechQuiz.WebApp/Controllers/SurveyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iTechArt.iTechQuiz.WebApp.Controllers
+{
+    public class SurveyController : Controller
+    {
+        public IActionResult NewSurvey()
+        {
+            return View();
+        }
+    }
+}
