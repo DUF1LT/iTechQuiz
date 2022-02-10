744ae87b7cb8bc460741d62e2a2e339bf98744af
Vadim <duf1lt@yandex.ru>
2022-02-10 11:40:43 +0300

    add ability to pass survey, answers are saved at db

--- iTechArt.iTechQuiz.Domain/Models/Answer.cs
+++ iTechArt.iTechQuiz.Domain/Models/Answer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using iTechArt.Repositories;
 
 namespace iTechArt.iTechQuiz.Domain.Models
--- iTechArt.iTechQuiz.Repositories/Migrations/20220210080444_pass survey.Designer.cs
+++ iTechArt.iTechQuiz.Repositories/Migrations/20220210080444_pass survey.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using iTechArt.iTechQuiz.Repositories.Context;
 
 namespace iTechArt.iTechQuiz.Repositories.Migrations
 {
     [DbContext(typeof(iTechQuizContext))]
-    partial class iTechQuizContextModelSnapshot : ModelSnapshot
+    [Migration("20220210080444_pass survey")]
+    partial class passsurvey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -278,6 +280,9 @@ namespace iTechArt.iTechQuiz.Repositories.Migrations
                         .HasColumnType("bit");
 
                     b.Property<bool>("IsAnonymous")
+                        .HasColumnType("bit");
+
+                    b.Property<bool>("IsDeleted")
                         .HasColumnType("bit");
 
                     b.Property<DateTime>("LastModifiedAt")
